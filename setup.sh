ae984500b930eae065169cc8dbccf93f20073679
Neil Weidinger <neilweidingern@gmail.com>
2020-05-29 00:45:11 +0100

    Make symlinks with full paths

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# get absolute path to dotfiles directory
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd | sed 's/\(^.*dotfiles\).*/\1/')"
+
 # install command line tools
 xcode-select -p >/dev/null 2>&1 || xcode-select --install
 
@@ -15,25 +18,25 @@ if [ ! -d "~/.tmux/plugins/tpm" ]; then
 fi
 
 # tmux config file
-ln -snfv tmux/tmux.conf ~/.tmux.conf
+ln -snfv $DIR/tmux/tmux.conf ~/.tmux.conf
 
 # git config
-ln -snfv git/gitconfig ~/.gitconfig
+ln -snfv $DIR/git/gitconfig ~/.gitconfig
 
 # git prompt and completion scripts
-ln -snfv git/git-completion.bash ~/git-completion.bash
-ln -snfv git/git-prompt.sh ~/git-prompt.sh
+ln -snfv $DIR/git/git-completion.bash ~/git-completion.bash
+ln -snfv $DIR/git/git-prompt.sh ~/git-prompt.sh
 
 # config directories
-ln -snfv config/bat ~/.config/bat
-ln -snfv config/karabiner ~/.config/karabiner
-ln -snfv config/neofetch ~/.config/neofetch
-ln -snfv config/neovim-config ~/.config/nvim
-ln -snfv config/ranger-config ~/.config/ranger
+ln -snfv $DIR/config/bat ~/.config/bat
+ln -snfv $DIR/config/karabiner ~/.config/karabiner
+ln -snfv $DIR/config/neofetch ~/.config/neofetch
+ln -snfv $DIR/config/neovim-config ~/.config/nvim
+ln -snfv $DIR/config/ranger-config ~/.config/ranger
 
 # install fzf bash integrations
 $(brew --prefix)/opt/fzf/install
 
 # bash profile
-ln -snfv bash_profile ~/.bash_profile
+ln -snfv $DIR/bash_profile ~/.bash_profile
 source ~/.bash_profile
