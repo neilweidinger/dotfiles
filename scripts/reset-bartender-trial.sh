90ff7b8d7a2748b33724fdc045fb0606086dd1da
Neil Weidinger <neilweidingern@gmail.com>
2020-06-03 18:52:35 +0100

    Add comment to bartender trial date reset script

--- scripts/reset-bartender-trial.sh
+++ scripts/reset-bartender-trial.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+# this sets the trial date to be one day before the current date
 defaults write com.surteesstudios.Bartender trialStart3 -date "$(date -v -1d +"%Y-%m-%d %I:%M:%S %z")"
