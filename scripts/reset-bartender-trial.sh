09315c5e0b07e6df862cddb3e410997a5830ad6c
Neil Weidinger <neilweidingern@gmail.com>
2020-06-26 17:22:17 +0100

    Make shebang more portable

--- scripts/reset-bartender-trial.sh
+++ scripts/reset-bartender-trial.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # this sets the trial date to be one day before the current date
 defaults write com.surteesstudios.Bartender trialStart3 -date "$(date -v -1d +"%Y-%m-%d %I:%M:%S %z")"
--- scripts/start-screensaver.sh
+++ scripts/start-screensaver.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 /System/Library/CoreServices/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine
