94473c3c8f89859ea7dd03199ec3a5291610d6fa
Neil Weidinger <neilweidingern@gmail.com>
2021-04-24 11:29:35 +0100

    Add comment explaining how to set up system wide keyboard shortcut to run screensaver script

--- scripts/start-screensaver.sh
+++ scripts/start-screensaver.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# To set up system wide mac keyboard shortcut to run script:
+# https://apple.stackexchange.com/a/256747/374139
 /System/Library/CoreServices/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine
